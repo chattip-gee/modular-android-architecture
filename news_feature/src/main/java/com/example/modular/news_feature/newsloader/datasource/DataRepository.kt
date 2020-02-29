ee51fb446ed6990f0388f6050595557879609323
Chattip Soontaku <chattip@ekoapp.com>
2020-02-29 22:16:18 +0700

    Created example modular architecture

---
+++ news_feature/src/main/java/com/example/modular/news_feature/newsloader/datasource/DataRepository.kt
@@ -0,0 +1,14 @@
+package com.example.modular.news_feature.newsloader.datasource
+
+import com.example.modular.news_feature.newsloader.model.NewsDataClass
+
+object DataRepository {
+    fun getNews(): List<NewsDataClass> {
+        val output = ArrayList<NewsDataClass>()
+        (1..5).forEach { index ->
+            output.add(NewsDataClass(index, "News Title $index", "News Description $index", index * 2))
+        }
+
+        return output
+    }
+}
---
+++ news_feature/src/main/java/com/example/modular/news_feature/newsloader/model/NewsDataClass.kt
@@ -0,0 +1,14 @@
+package com.example.modular.news_feature.newsloader.model
+
+
+data class NewsDataClass(
+
+    val id: Int,
+
+    val title: String,
+
+    val description: String,
+
+    val userId: Int
+)
+
